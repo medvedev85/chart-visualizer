2cbb828224e402988e602d569d5d7b032e70ce15
medvedev85 <b.a.medvedev@gmail.com>
2020-04-21 07:10:57 +0500

    исправлена ошибка в методе createChart, многие функции перенесены в класс

--- ChartDrawer.js
+++ ChartDrawer.js
@@ -5,16 +5,17 @@ class ChartDrawer {
         this.ctx = params.ctx;
         this.motifColors = params.motifColors;
         this.leftBorder = params.leftBorder;
-        this.rightBorder = params.rightBorder;
+        this.lineWidth = params.lineWidth;
         this.marginTop = params.marginTop;
         this.stepLine = params.stepLine;
         this.rectHeight = params.rectHeight;
         this.minSizeRect = params.minSizeRect;
+        this.rightBorder = this.lineWidth + this.leftBorder;
         this.rects = [];
         this.json = null;
         this.coordinate = {};
         this.names = [];
-        
+
     }
     nameSequence() {
         this.sequences = this.json.sequences;
@@ -44,26 +45,34 @@ class ChartDrawer {
             }
         }
     }
+    getHeight() {
+        if (this.json.sequences) {
+            canvas.height = this.marginTop + this.stepLine * (this.names.length + 1);
+
+        }
+        return canvas.height;
+    }
     createChart() {
         for (let i = 0; i < this.rangesFullData.length; i++) {
             for (let j = 0; j < this.names.length; j++) {
                 if (this.rangesFullData[i].sequenceName == this.names[j]) {
                     this.name = this.names[j];
-                }
 
-                this.long = (this.rightBorder - this.leftBorder) / this.rangesFullData[i].sequenceLenght;
-                if (this.rangesFullData[i].complementary == 1) {
-                    this.long = (this.rightBorder - this.leftBorder) / this.rangesFullData[i].complementarySequenceLenght;
+
+                    this.long = (this.rightBorder - this.leftBorder) / this.rangesFullData[i].sequenceLenght;
+                    if (this.rangesFullData[i].complementary == 1) {
+                        this.long = (this.rightBorder - this.leftBorder) / this.rangesFullData[i].complementarySequenceLenght;
+                    }
+                    this.ctx.fillStyle = "blue";
+                    this.rects[i] = {};
+                    this.rects[i].x = Math.ceil(this.rangesFullData[i].start * this.long + this.leftBorder);
+                    this.rects[i].w = Math.floor((this.rangesFullData[i].end * this.long + this.leftBorder) - this.rects[i].x);
+                    this.rects[i].w = this.rects[i].w >= this.minSizeRect ? this.rects[i].w : this.minSizeRect;
+                    this.rects[i].h = this.rectHeight;
+                    this.rects[i].y = this.marginTop - this.rectHeight + this.stepLine * this.name;
+                    this.ctx.rect(this.rects[i].x, this.rects[i].y, this.rects[i].w, this.rects[i].h);
+                    this.ctx.fill();
                 }
-                this.ctx.fillStyle = "blue";
-                this.rects[i] = {};
-                this.rects[i].x = Math.ceil(this.rangesFullData[i].start * this.long + this.leftBorder);
-                this.rects[i].w = Math.floor((this.rangesFullData[i].end * this.long + this.leftBorder) - this.rects[i].x);
-                this.rects[i].w = this.rects[i].w >= this.minSizeRect ? this.rects[i].w : this.minSizeRect;
-                this.rects[i].h = this.rectHeight;
-                this.rects[i].y = this.marginTop - this.rectHeight + this.stepLine * this.name;
-                this.ctx.rect(this.rects[i].x, this.rects[i].y, this.rects[i].w, this.rects[i].h);
-                this.ctx.fill();
             }
         }
     }
@@ -73,6 +82,26 @@ class ChartDrawer {
             this.ctx.moveTo(this.leftBorder, this.marginTop + this.stepLine * i);
             this.ctx.lineTo(this.rightBorder, this.marginTop + this.stepLine * i);
             this.ctx.stroke();
+        }
+    }
+    addChart() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.json = JSON.parse(this.input.value);
+        canvas.width = this.leftBorder + this.rightBorder;
+        canvas.height = 0;
+        this.nameSequence();
+        this.fillRangesFullData();
+        this.getHeight();
+        this.paintLine();
+        setLineDescription();
+        this.createChart();
+        document.getElementById('headerCanvas').style.display = 'block';
+
+        if (typeof this.json == "object") {
+            document.getElementById('result').innerHTML = "";
+
+        } else {
+            document.getElementById('result').innerHTML = " Поместите данные в формате JSON!";
         }
     }
 }
--- main.js
+++ main.js
@@ -6,53 +6,24 @@ let params = {
     ctx: canvas.getContext("2d"),
     motifColors: ["blue", "red", "yellow", "pink", "green"],
     leftBorder: 100,
-    rightBorder: 1000,
+    lineWidth: 1000,
     marginTop: 100,
     stepLine: 50,
     rectHeight: 10,
     minSizeRect: 20
 }
 
+
 let chartDrawer = new ChartDrawer(params);
 
 //chartDrawer.input.onpaste
 chartDrawer.input.oninput = function () {
-    chartDrawer.ctx.clearRect(0, 0, chartDrawer.canvas.width, chartDrawer.canvas.height);
-    chartDrawer.json = JSON.parse(chartDrawer.input.value);
-
-    chartDrawer.nameSequence();
-    chartDrawer.fillRangesFullData();
-    getHeight();
-    chartDrawer.paintLine();
-    setLineDescription();
-    chartDrawer.createChart();
-    document.getElementById('hederCanvas').style.display = 'block';
-
-    if (typeof chartDrawer.json == "object") {
-        document.getElementById('result').innerHTML = "";
-        
-    } else {
-        document.getElementById('result').innerHTML = " Поместите данные в формате JSON!";
-    }
+    chartDrawer.addChart();
 }
-
-//document.getElementById('space').innerHTML = chartDrawer.leftBorder;
 
 chartDrawer.canvas.onmousemove = function (e) {
     chartDrawer.coordinate.x = e.offsetX;
     chartDrawer.coordinate.y = e.offsetY;
-   // chartDrawer.ctx.clearRect(0, 0, chartDrawer.canvas.width, chartDrawer.canvas.height);
-}
-
-canvas.width = chartDrawer.leftBorder + chartDrawer.rightBorder;
-canvas.height = 0;
-
-function getHeight() {
-    if (chartDrawer.json.sequences) {
-        canvas.height = chartDrawer.marginTop + chartDrawer.stepLine * (chartDrawer.names.length + 1);
-
-    } 
-    return canvas.height;
 }
 
 function setLineDescription() {
