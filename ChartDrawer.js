872ca9320281f533f44e1e27ffdf9bf8bf240cf1
medvedev85 <b.a.medvedev@gmail.com>
2020-04-28 13:40:44 +0500

    поправлены некоторые недочеты

--- ChartDrawer.js
+++ ChartDrawer.js
@@ -1,6 +1,6 @@
 class ChartDrawer {
     constructor(params) {
-        this.canvas = params.canvas;
+        this.canvas = document.getElementById(params.canvas);
         this.ctx = this.canvas.getContext("2d");
         this.ctx = params.ctx;
         this.motifColors = params.motifColors;
@@ -122,4 +122,27 @@ class ChartDrawer {
             }
         }
     }
+}
+
+function parser(inputData, params) {
+    let rects = [];
+    for (let i = 0; i < inputData.motifs.length; i++) {
+        for (let j = 0; j < inputData.motifs[i].occurences.length; j++) {
+            for (let k = 0; k < inputData.motifs[i].occurences[j].ranges.length; k++) {
+                inputData.motifs[i].occurences[j].ranges[k].motif = inputData.motifs[i].motif;
+                inputData.motifs[i].occurences[j].ranges[k].sequenceName = inputData.motifs[i].occurences[j].sequence_name;
+                rects.push(inputData.motifs[i].occurences[j].ranges[k]);
+            }
+        }
+    }
+    for (let i = 0; i < inputData.sequences.length; i++) {
+        inputData.sequences[i].rects = [];
+        for (let j = 0; j < rects.length; j++) {
+            if (inputData.sequences[i].name == rects[j].sequenceName) {
+                inputData.sequences[i].rects.push(rects[j]);
+            }
+        }
+    }
+    console.log(params);
+    return params.segments = inputData.sequences;
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict"
 window.onload = function () {
     let input = document.getElementById("json_input_id");
-    let result = 'result';
+    let result = document.getElementById('result');
     let params = {
-        canvas: document.getElementById("canvas"),
+        canvas: "canvas",
         motifColors: ["blue", "red", "yellow", "pink", "green"],
         leftBorder: 100,
         lineWidth: 1000,
@@ -12,41 +12,17 @@ window.onload = function () {
         rectHeight: 15,
         minSizeRect: 20
     };
-    halper(input, params, result);
+    initChart(input, params, result);
 }
 
-
-function parser(inputData, params) {
-    let rects = [];
-    for (let i = 0; i < inputData.motifs.length; i++) {
-        for (let j = 0; j < inputData.motifs[i].occurences.length; j++) {
-            for (let k = 0; k < inputData.motifs[i].occurences[j].ranges.length; k++) {
-                inputData.motifs[i].occurences[j].ranges[k].motif = inputData.motifs[i].motif;
-                inputData.motifs[i].occurences[j].ranges[k].sequenceName = inputData.motifs[i].occurences[j].sequence_name;
-                rects.push(inputData.motifs[i].occurences[j].ranges[k]);
-            }
-        }
-    }
-    for (let i = 0; i < inputData.sequences.length; i++) {
-        inputData.sequences[i].rects = [];
-        for (let j = 0; j < rects.length; j++) {
-            if (inputData.sequences[i].name == rects[j].sequenceName) {
-                inputData.sequences[i].rects.push(rects[j]);
-            }
-        }
-    }
-    console.log(params);
-    return params.segments = inputData.sequences;
-}
-
-function halper(input, params, result) {
+function initChart(input, params, result, chartDrawer) {
     input.oninput = () => {
-        let json = JSON.parse(input.value);
         try {
+            let json = JSON.parse(input.value);
             parser(json, params);
-            document.getElementById(result).innerHTML = "";
+            result.innerHTML = "";
         } catch (error) {
-            document.getElementById(result).innerHTML = " Поместите данные в формате JSON!";
+            result.innerHTML = " Поместите данные в формате JSON!";
         }
     }
 }
